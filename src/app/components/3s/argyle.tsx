c975323bb07d25c4d39a5231bd43ca1a4e7d6a2b
Eric Heidel <ericleeheidel@yahoo.com>
2024-09-28 16:31:05 -0500

    some edits and added Concave Sunset to Desktop version

---
+++ src/app/components/3s/argyle.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+
+export const ArgyleDesktop = () => {
+  return (
+    <div>
+      <div className="flex flex-row flex-wrap w-fit rounded-xl border-2 border-[#1BFFFF] ">
+        <div>
+          <h2 className="my-2 text-center">&quot;Argyle 1, 2 & 3&quot;</h2>
+          <Image
+            src="/assets/paintings/argyle_1_2_3.png"
+            alt="An image of a 3 paintings of argyle patterns"
+            width={230}
+            height={230}
+            priority
+            unoptimized
+            className="px-4 pb-4 shadow-xl"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const ArgyleMobile = () => {
+  return (
+    <div>
+      <div>Argyle Mobile</div>
+    </div>
+  )
+}
